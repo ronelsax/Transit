a6e0d7b3f49088983f4ed90047f78ce0c15ffdc0
Chris Sano <csano@microsoft.com>
2014-07-09 18:41:14 -0700

    Initial Commit

---
+++ Transit/Transit.h
@@ -0,0 +1,13 @@
+//
+//  Transit.h
+//  Transit
+//
+//  Created by Chris Sano on 7/9/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Transit : NSObject
+
+@end
---
+++ Transit/Transit.m
@@ -0,0 +1,13 @@
+//
+//  Transit.m
+//  Transit
+//
+//  Created by Chris Sano on 7/9/14.
+//
+//
+
+#import "Transit.h"
+
+@implementation Transit
+
+@end
